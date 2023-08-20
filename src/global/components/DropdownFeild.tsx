0387e91799290c765f17d08cf9b8e91daa086ee1
wiaam-kareem <wiaamkareem93@gmail.com>
2023-08-20 15:34:44 +0300

    update phone feild

--- src/global/components/DropdownFeild.tsx
+++ src/global/components/DropdownFeild.tsx
@@ -15,9 +15,28 @@ const DropdownFeild = ({optionsList,text,name}:{optionsList:Array<string>,text:s
             >
             <option disabled={true} value="">اختر</option>
             {optionsList.map((item)=> (<option key={item} value={item}>{item}</option>))}</Field>
-            <ErrorMessage name={name} component="div" />
+            <ErrorMessage name={name} render={renderErrorMessage} />
               </div>
             </div>  )
 }
 
 export default DropdownFeild
+const renderErrorMessage = (errormsg: string) => (
+    <div className="flex flex-row items-end justify-end py-4">
+      <span className="mx-2 text-sm text-gray2">{errormsg}</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6 text-red-400 text-sm"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+        />
+      </svg>
+    </div>
+  );
